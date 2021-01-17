cbc24d6c8293f11c9f823bd41f06f7e5faa2af6e
lukasz laba <lukaszlaba@gmail.com>
2021-01-17 17:37:59 +0100

    'update'

--- logeweb/urls.py
+++ logeweb/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from logeweb.views import hello
+from django.urls import path, include, re_path
+from logeweb.views import hello, index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
     path('scripts/', include('scripts.urls')),
+    re_path(r'^$', index, name='index'),
 ]
--- logeweb/views.py
+++ logeweb/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 def hello(request):
    text = """<h1>welcome to my apAp !</h1>"""
    return HttpResponse(text)
+
+def index(request):
+   text = """
+   <h1>Logehub start page !</h1>
+   <a href="/scripts/list/" > Go to script list </a>
+   """
+   return HttpResponse(text)
+
