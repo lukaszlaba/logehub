cfe7a3e57e3398fbe414ac68d814966688a29c7e
lukasz laba <lukaszlaba@gmail.com>
2021-01-20 22:01:20 +0100

    'update'

--- logeweb/settings.py
+++ logeweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['logehub.herokuapp.com']
 
 
 # Application definition
